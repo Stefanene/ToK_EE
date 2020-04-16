c38e95bc86b161ed1dbe1cc7887b188a8e09c5f2
Stefanene <stefanene@hotmail.com>
2020-04-16 14:59:12 -0700

    added timer

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <ctime>
 
 using namespace std;
 
@@ -11,15 +12,23 @@ void toBinary(char input);
 char message[101];
 int binaryTrans[701];
 char key[101];
-ind encrypted[701];
+int encrypted[701];
 int decrypted[701];
 char output[101];
 
 int main() {
   //variables
-
+  clock_t time;
+  time = clock();
   //program
-  
-  
+  int i = 1;
+  int c = 0;
+  while (c < 10000000) {
+    c++;
+    i*60/79;
+  }
+  //end program
+  time = clock() - time;
+  cout << "Time to execute: " << (float)time/CLOCKS_PER_SEC << " seconds" << endl;
   return 0;
 }
