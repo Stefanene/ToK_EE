9ed56f3c212a950b8fcf1cef9d778a06106e7b12
Stefanene <stefanene@hotmail.com>
2020-04-16 14:27:04 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+//functions
+void toBinary(char input);
+
+
+//global variables
+char message[101];
+int binaryTrans[701];
+char key[101];
+ind encrypted[701];
+int decrypted[701];
+char output[101];
+
+int main() {
+  //variables
+
+  //program
+  
+  
+  return 0;
+}
