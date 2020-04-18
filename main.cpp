970a3dd037a1f8278803e045b8785b136ec7055f
Stefanene <stefanene@hotmail.com>
2020-04-17 17:40:44 -0700

    added binary translator

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 //functions
-void toBinary(char input);
+void toBinary();
 
 
 //global variables
@@ -18,17 +18,42 @@ char output[101];
 
 int main() {
   //variables
+
+  //set up time
   clock_t time;
   time = clock();
   //program
-  int i = 1;
-  int c = 0;
-  while (c < 10000000) {
-    c++;
-    i*60/79;
-  }
-  //end program
+  cout << "Input message:  ";
+  cin.get(message, 101);
+  cin.clear();
+  cin.ignore(10000, '\n');
+  toBinary();
+  //end program and timer
   time = clock() - time;
   cout << "Time to execute: " << (float)time/CLOCKS_PER_SEC << " seconds" << endl;
   return 0;
+}
+
+void toBinary() {
+  int count, i = 0;
+  while ((int)message[count] >= 32 && (int)message[count] <= 127) {
+    int add[7];
+    int n = (int)message[count];
+    for(i = 0; n > 0; i++) {    
+      add[i]=n % 2;    
+      n = n/2;  
+    }
+    //add to binary array
+    for(int a = 0; a < 7; a++) {
+      binaryTrans[7*count+a] = add[a];
+    }
+    count++;
+  }
+  //display message in binary
+  int d = 0;
+  while(d < (7*count)) {
+    cout << binaryTrans[d];
+    d++;
+  }
+  cout << endl;
 }
